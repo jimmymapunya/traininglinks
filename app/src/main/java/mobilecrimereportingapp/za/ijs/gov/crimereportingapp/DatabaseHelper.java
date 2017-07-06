9be52c450337a07f2cce0181b4dca659642a60c8
MaribolleR <mariboller@ijs.gov.za>
2017-07-06 09:36:19 +0200

    Toast added. Database Helper class modified.

--- app/src/main/java/mobilecrimereportingapp/za/ijs/gov/crimereportingapp/DatabaseHelper.java
+++ app/src/main/java/mobilecrimereportingapp/za/ijs/gov/crimereportingapp/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package mobilecrimereportingapp.za.ijs.gov.crimereportingapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -11,21 +12,33 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper
 {
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public static final String DB_NAME = "DashboardDB.db";
+    public static final String TABLE_NAME = "dashboard_data_table";
+    public static final String COL_1 = "notificationCount";
+    public static final String COL_2 = "inboxCount";
+    public static final String COL_3 = "myCaseCount";
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, 1 );
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(NOTIFCOUNT INTEGER, INBOXCOUNT INTEGER, CASECOUNT INTEGER)");
     }
 
-    public Cursor getData(){
-        SQLiteDatabase database = this.getWritableDatabase();
-        return  null;
-    }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
 
+    public boolean insertData(int notificationCount, int inboxCount, int caseCount){
+
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        //contentValues.put();
+
+        return true;
     }
 }
