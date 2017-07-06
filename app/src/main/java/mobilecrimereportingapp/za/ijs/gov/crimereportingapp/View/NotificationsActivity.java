cf101736d78ee7d35128fcfe423c47f22c08d186
Jimmy Mapunya <jimmymapunya@gmail.com>
2017-07-06 09:58:15 +0200

    Test message

--- app/src/main/java/mobilecrimereportingapp/za/ijs/gov/crimereportingapp/View/NotificationsActivity.java
+++ app/src/main/java/mobilecrimereportingapp/za/ijs/gov/crimereportingapp/View/NotificationsActivity.java
@@ -1,8 +1,10 @@
-package mobilecrimereportingapp.za.ijs.gov.crimereportingapp;
+package mobilecrimereportingapp.za.ijs.gov.crimereportingapp.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+
+import mobilecrimereportingapp.za.ijs.gov.crimereportingapp.R;
 
 public class NotificationsActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class NotificationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+
+        //Test Message
+
+
     }
 }
