dacc0555cbc6db2a23749d7de012a92c7c850460
Thato Makola <thatofrancis@outlook.com>
2017-07-07 14:59:01 +0200

    Merge remote-tracking branch 'origin/MaribolleR' into MaribolleR
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/mobilecrimereportingapp/za/ijs/gov/crimereportingapp/Controller/DatabaseHelper.java
    #	app/src/main/java/mobilecrimereportingapp/za/ijs/gov/crimereportingapp/DatabaseHelper.java
    #	app/src/main/java/mobilecrimereportingapp/za/ijs/gov/crimereportingapp/MainActivity.java
    #	app/src/main/java/mobilecrimereportingapp/za/ijs/gov/crimereportingapp/View/MainActivity.java
    #	app/src/main/java/mobilecrimereportingapp/za/ijs/gov/crimereportingapp/controller/DatabaseHelper.java
    #	app/src/main/java/mobilecrimereportingapp/za/ijs/gov/crimereportingapp/view/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml

---
+++ app/src/main/java/mobilecrimereportingapp/za/ijs/gov/crimereportingapp/view/ProvideFeedback.java
@@ -0,0 +1,25 @@
+package mobilecrimereportingapp.za.ijs.gov.crimereportingapp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mobilecrimereportingapp.za.ijs.gov.crimereportingapp.R;
+
+/**
+ * Created by ThatoM on 2017/07/07.
+ * This is the class for selecting the type of feedback to give
+ */
+
+public class ProvideFeedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //lets commit first try
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provide_feedback);
+
+
+    }
+
+
+}
