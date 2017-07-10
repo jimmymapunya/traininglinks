4b7faf1f4f669c27956974d90358201d3c53e232
Jimmy Mapunya <jimmymapunya@gmail.com>
2017-07-10 11:17:54 +0200

    Added report crime class and layout

---
+++ app/src/main/java/mobilecrimereportingapp/za/ijs/gov/crimereportingapp/view/ReportCrimeActivity.java
@@ -0,0 +1,61 @@
+package mobilecrimereportingapp.za.ijs.gov.crimereportingapp.view;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import mobilecrimereportingapp.za.ijs.gov.crimereportingapp.R;
+
+public class ReportCrimeActivity extends AppCompatActivity {
+
+    private android.support.v7.widget.Toolbar Toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_crime);
+
+        /*Toolbar and Buttons instantiation*/
+        Toolbar = (Toolbar) findViewById(R.id.appBar);
+        Toolbar.setTitle("Report Crime");
+
+
+        setSupportActionBar(Toolbar);
+
+        /*Back icon for navigation drawer*/
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationDrawerFrag navigationDrawerFrag = (NavigationDrawerFrag)
+                getSupportFragmentManager().findFragmentById(R.id.frag_nav_drawer);
+
+        navigationDrawerFrag.setUpDrawer(R.id.frag_nav_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), Toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+
+            return true;
+        } else if (id == R.id.notifications) {
+            //startActivity(new Intent(this, NotificationsActivity.class));
+
+        } else if (id == R.id.inbox) {
+            /*Add some inbox code to redirect*/
+        }
+
+        return false;
+
+    }
+}
