44c8b6b835ed43a101cb5daac1b029ca0ffea408
MaribolleR <mariboller@ijs.gov.za>
2017-07-12 09:39:15 +0200

    Crime Reporting screen almost finished.

--- app/src/main/java/mobilecrimereportingapp/za/ijs/gov/crimereportingapp/view/ReportCrimeActivity.java
+++ app/src/main/java/mobilecrimereportingapp/za/ijs/gov/crimereportingapp/view/ReportCrimeActivity.java
@@ -11,9 +11,18 @@ import android.widget.Spinner;
 
 import mobilecrimereportingapp.za.ijs.gov.crimereportingapp.R;
 
+/**
+ * Created by MaribolleR on 2017/07/01.
+ * Purpose: Crime reporting activity for the Crime reporting app.
+ */
+
 public class ReportCrimeActivity extends AppCompatActivity {
 
     private android.support.v7.widget.Toolbar Toolbar;
+
+    private Spinner spinnerKnowOffender, spinnerInjuries, spinnerSceneItems, spinnerWeapons;
+    private ArrayAdapter<CharSequence> spinnerKnowOffenderAdapter, spinnerInjuriesAdapter, spinnerSceneItemsAdapter, spinnerWeaponsAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,18 +32,7 @@ public class ReportCrimeActivity extends AppCompatActivity {
         Toolbar = (Toolbar) findViewById(R.id.appBar);
         Toolbar.setTitle("Report Crime");
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.injuries_choice, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-
         setSupportActionBar(Toolbar);
-
         /*Back icon for navigation drawer*/
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -42,6 +40,34 @@ public class ReportCrimeActivity extends AppCompatActivity {
                 getSupportFragmentManager().findFragmentById(R.id.frag_nav_drawer);
 
         navigationDrawerFrag.setUpDrawer(R.id.frag_nav_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), Toolbar);
+
+        /*Starting code for the spinners*/
+        spinnerKnowOffender = (Spinner) findViewById(R.id.spinnerKnowOffender);
+        spinnerInjuries = (Spinner) findViewById(R.id.spinnerInjuries);
+        spinnerSceneItems = (Spinner) findViewById(R.id.spinnerSceneItems);
+        spinnerWeapons = (Spinner) findViewById(R.id.spinnerWeapons);
+
+        /* Create ArrayAdapters using the string array and a default spinner layout*/
+        spinnerKnowOffenderAdapter = ArrayAdapter.createFromResource(this,
+                R.array.injuries_choice, android.R.layout.simple_spinner_item);
+        spinnerInjuriesAdapter = ArrayAdapter.createFromResource(this,
+                R.array.injuries_choice, android.R.layout.simple_spinner_item);
+        spinnerSceneItemsAdapter = ArrayAdapter.createFromResource(this,
+                R.array.injuries_choice, android.R.layout.simple_spinner_item);
+        spinnerWeaponsAdapter = ArrayAdapter.createFromResource(this,
+                R.array.injuries_choice, android.R.layout.simple_spinner_item);
+
+        /* Specify the layout to use when the list of choices appears*/
+        spinnerKnowOffenderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerInjuriesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSceneItemsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerWeaponsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        /* Apply the adapter to the spinner*/
+        spinnerKnowOffender.setAdapter(spinnerKnowOffenderAdapter);
+        spinnerInjuries.setAdapter(spinnerInjuriesAdapter);
+        spinnerSceneItems.setAdapter(spinnerSceneItemsAdapter);
+        spinnerWeapons.setAdapter(spinnerWeaponsAdapter);
 
     }
 
